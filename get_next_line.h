af09244753489cad44da3bfed92b44b393d301aa
Youssef El-Hamrani <yel-hamr@c3r9p5.1337.ma>
2024-12-10 19:10:06 +0100

    version 0.0.0

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-hamr <yel-hamr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/10 18:06:36 by yel-hamr          #+#    #+#             */
+/*   Updated: 2024/12/10 18:11:13 by yel-hamr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h> 
+
+#define BUFFER_SIZE 17
+
+
+#endif
